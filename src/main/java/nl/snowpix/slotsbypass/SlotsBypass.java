5e46cbac5b570a9537f88642be7d83473411bcbc
Jona Woning <30323293+jonawoning@users.noreply.github.com>
2021-02-06 13:15:47 +0100

    Add files via upload

---
+++ src/main/java/nl/snowpix/slotsbypass/SlotsBypass.java
@@ -0,0 +1,36 @@
+package nl.snowpix.slotsbypass;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SlotsBypass extends JavaPlugin {
+
+    public static SlotsBypass instance;
+    public FileConfiguration c;
+    public String Prefix;
+    public String BypassPermission;
+    public String Message_Format;
+    public boolean BypassMessage;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        c = getConfig();
+        Prefix = ChatColor.translateAlternateColorCodes('&', instance.c.getString("Prefix"));
+        BypassPermission = instance.c.getString("BypassPermission");
+        Message_Format = ChatColor.translateAlternateColorCodes('&', instance.c.getString("Message_Format"));
+        BypassMessage = instance.c.getBoolean("BypassMessage");
+        getServer().getPluginManager().registerEvents(new onJoin(), this);
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        System.out.println("[BypassSlots] The plugin is succesfully enabled.");
+
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[BypassSlots] The plugin is succesfully disabled.");
+    }
+
+}
